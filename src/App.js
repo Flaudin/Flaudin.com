1d7e830551bea6b90a0fe33801b57f03fe29125d
Flaudin <2016-1362751@asiancollege.edu.ph>
2022-08-25 20:55:09 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import { NavBar } from './components/NavBar';
+import { Banner } from './components/Ban';
+import { Skills } from './components/Skills';
+import {Projects} from './components/Project';
+import {Contact} from './components/Contact';
+// import { Footer } from "./components/Footer";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar/>
+      <Banner/>
+      <Skills/>
+      <Projects/>
+      <Contact/>
+    </div>
+  );
+}
+
+export default App;
