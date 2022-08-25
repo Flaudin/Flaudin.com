a8e49c3714542e694e1e007f1cdaa9f0db458a3b
Flaudin <2016-1362751@asiancollege.edu.ph>
2022-08-26 07:51:58 +0800

    footer added & added icons

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import './App.css';
-import { NavBar } from './components/NavBar';
-import { Banner } from './components/Ban';
-import { Skills } from './components/Skills';
-import {Projects} from './components/Project';
-import {Contact} from './components/Contact';
-// import { Footer } from "./components/Footer";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
+import { NavBar } from "./components/NavBar";
+import { Banner } from "./components/Ban";
+import { Skills } from "./components/Skills";
+import { Projects } from "./components/Project";
+import { Contact } from "./components/Contact";
+import { Footer } from "./components/Footer";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
-      <NavBar/>
-      <Banner/>
-      <Skills/>
-      <Projects/>
-      <Contact/>
+      <NavBar />
+      <Banner />
+      <Skills />
+      <Projects />
+      <Contact />
+      <Footer />
     </div>
   );
 }
